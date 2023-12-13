6310703ed1d3fd2a0a2c36c49224803139e0697b
gsus <gsus.alcaraz@gmail.com>
2023-12-13 20:55:51 +0100

    Dialogo creador de tareas

---
+++ app/src/main/java/com/jalcaraz/todoapp/addtasks/ui/TasksViewModel.kt
@@ -0,0 +1,8 @@
+package com.jalcaraz.todoapp.addtasks.ui
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+// @HiltViewModel
+class TasksViewModel @Inject constructor() : ViewModel() {
+}
